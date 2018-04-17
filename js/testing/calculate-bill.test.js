c5a39b672042e68cd7d0f1b9ad8c19f5ae685c49
Thembisile <shaundamon6@icloud.com>
2018-04-17 10:26:46 +0200

    testing for the calculate bill

---
+++ js/testing/calculate-bill.test.js
@@ -0,0 +1,11 @@
+describe('calculateBtnClicked', function(){
+  it('should calculate the total bill string of calls made', function(){
+    assert.equal(calculateBtnClicked('call'), '2.75');
+  })
+  it('should calculate the total bill string of sms made', function(){
+    assert.equal(calculateBtnClicked('sms'), '0.75');
+  })
+  it('should calculate the total bill string of calls and sms made', function(){
+    assert.equal(calculateBtnClicked('call','sms'), '2.75','0.75');
+  })
+})
